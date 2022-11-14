8b380f37fb60973e5596cc87082368e2c02b2b67
ridhoadtyaa <r2603an@gmail.com>
2022-11-14 12:50:22 +0700

    feat: implement sales recap data

--- pages/dashboard/sales-recap.jsx
+++ pages/dashboard/sales-recap.jsx
@@ -1,76 +1,78 @@
-import Image from 'next/image';
-import React from 'react';
-import { MainLayout } from "../../components/layouts"
-// import { FaTrashAlt } from 'react-icons/fa';
-// import { PrimaryButton } from '../components/button';
-// import Link from 'next/link';
-// import { BsArrowLeft } from 'react-icons/bs';
+import { useSelector } from 'react-redux';
+import { DashboardLayout } from '../../components/layouts';
+import { JWT_SECRET } from '../../utils/env';
+import jwt from 'jsonwebtoken';
+import { useMemo } from 'react';
+import useIsSSR from '../../hooks/useIsSSR';
 
 const RecapPage = () => {
-    return (
-        <MainLayout title="My Recap">
-            <section className="py-8">
-                <div className="flex items-center justify-between border-b border-gray-300 pb-6">
-                    <h3>Sales Recap</h3>
-                </div>
-                <br/>
-             <div className="rounded-lg shadow">
-                <table className="w-full ">
-                <thead className="bg-black border-b-3  border-gray-200 text-white">
-                    <tr>
-                    <th className="p-3 text-sm font-semibold tracking-wide text-left">Product</th>
-                    <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">Prices</th>
-                    <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">Sales</th>
-                    <th className="w-32 p-3 text-sm font-semibold tracking-wide text-left">Revenue</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className="bg-white">
-                    <td className="p-3 text-sm text-gray-700">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</td>
-                    <td className="p-3 text-sm text-gray-700" >$200</td>
-                    <td className="p-3 text-sm text-gray-700">1</td>
-                    <td className="p-3 text-sm text-gray-700 text-left">$200</td>
-                </tr>
-                    <tr className="bg-gray-200" >
-                    <td className="p-3 text-sm text-gray-700">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</td>
-                    <td className="p-3 text-sm text-gray-700">$200</td>
-                    <td className="p-3 text-sm text-gray-700">1</td>
-                    <td className="p-3 text-sm text-gray-700">$200</td>
-                </tr>
-                    <tr className="bg-white">
-                    <td className="p-3 text-sm text-gray-700">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptopsr</td>
-                    <td className="p-3 text-sm text-gray-700">$200</td>
-                    <td className="p-3 text-sm text-gray-700">1</td>
-                    <td className="p-3 text-sm text-gray-700">$200</td>
-                </tr>
-                    <tr className="bg-gray-200">
-                    <td className="p-3 text-sm text-gray-700">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptopsr</td>
-                    <td className="p-3 text-sm text-gray-700">$200</td>
-                    <td className="p-3 text-sm text-gray-700">1</td>
-                    <td className="p-3 text-sm text-gray-700">$200</td>
-                </tr>
-                <tr className="bg-white">
-                    <td className="p-3 text-sm text-gray-700">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptopsr</td>
-                    <td className="p-3 text-sm text-gray-700">$200</td>
-                    <td className="p-3 text-sm text-gray-700">1</td>
-                    <td className="p-3 text-sm text-gray-700">$200</td>
-                </tr>
-                <tr className="bg-gray-200">
-                    <td className="p-3 text-sm text-gray-700">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptopsr</td>
-                    <td className="p-3 text-sm text-gray-700">$200</td>
-                    <td className="p-3 text-sm text-gray-700">1</td>
-                    <td className="p-3 text-sm text-gray-700">$200</td>
-                </tr>
-                </tbody>
-                </table>
-                </div>
-                 <div className="pb-2 py-4">
-                 <td className="p-3 font-bold text-sm text-gray-700">Total Revenue : </td>
-                 <td className="p-3 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 rounded-lg bg-opacity-50">$200</td> 
-                 </div>
-               </section>
-              </MainLayout>
-            )
-        }
+	const salesRecap = useSelector(state => state.product.salesRecap);
+	const isSSR = useIsSSR();
+
+	const totalRevenue = useMemo(
+		() => salesRecap.reduce((previousValue, currentValue) => previousValue + currentValue.price * currentValue.quantity, 0),
+		[salesRecap]
+	);
+
+	return (
+		<DashboardLayout title="Sales Recap">
+			<h3>Sales Recap</h3>
+			{!isSSR && (
+				<section className="my-4">
+					<br />
+					<div className="rounded-lg shadow">
+						<table className="w-full">
+							<thead className="border-b-3 border-gray-200 bg-black text-white">
+								<tr>
+									<th className="p-3 text-left text-sm font-semibold tracking-wide">Product</th>
+									<th className="w-24 p-3 text-left text-sm font-semibold tracking-wide">Price</th>
+									<th className="w-24 p-3 text-left text-sm font-semibold tracking-wide">Sales</th>
+									<th className="w-32 p-3 text-left text-sm font-semibold tracking-wide">Revenue</th>
+								</tr>
+							</thead>
+							<tbody>
+								{salesRecap.length ? (
+									salesRecap.map(item => (
+										<tr className="bg-white" key={item.id}>
+											<td className="p-3 text-sm text-gray-700">{item.title}</td>
+											<td className="p-3 text-sm text-gray-700">${item.price}</td>
+											<td className="p-3 text-sm text-gray-700">{item.quantity}</td>
+											<td className="p-3 text-left text-sm text-gray-700">${item.price * item.quantity}</td>
+										</tr>
+									))
+								) : (
+									<p className="text-center">Nothing here</p>
+								)}
+							</tbody>
+						</table>
+					</div>
+					<div className="py-4 pb-2">
+						<td className="p-3 text-sm font-bold text-gray-700">Total Revenue : </td>
+						<td className="rounded-lg bg-green-200 bg-opacity-50 p-3 text-xs font-medium uppercase tracking-wider text-green-800">${totalRevenue}</td>
+					</div>
+				</section>
+			)}
+		</DashboardLayout>
+	);
+};
+
+export const getServerSideProps = ctx => {
+	const token = ctx.req.cookies['admin-token'];
+
+	try {
+		jwt.verify(token, JWT_SECRET);
+	} catch {
+		return {
+			redirect: {
+				destination: '/admin-login',
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {},
+	};
+};
 
 export default RecapPage;
